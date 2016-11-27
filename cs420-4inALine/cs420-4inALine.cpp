600c108e4f666e201cb5567955219037cdaaac6f
rwhere <rnwaer@cpp.edu>
2016-11-27 13:57:46 -0800

    check if theres a winner

--- cs420-4inALine/cs420-4inALine.cpp
+++ cs420-4inALine/cs420-4inALine.cpp
@@ -12,18 +12,27 @@ using namespace std;
 void initializeBoard(char board[][GRID_SIZE]);
 void printBoard(char board[][GRID_SIZE]);
 bool makeMove(char board[][GRID_SIZE], char player, string move);
+int checkForWinner(char board[][GRID_SIZE]);
+
 
 int main()
 {
 	char board[GRID_SIZE][GRID_SIZE];
 	initializeBoard(board);
 	printBoard(board);
+	makeMove(board, X, "a4");
 	makeMove(board, X, "a5");
-	printBoard(board);
+	makeMove(board, O, "a6");
+	makeMove(board, O, "b6");
+	makeMove(board, O, "c6");
 	makeMove(board, O, "d6");
+	makeMove(board, X, "a7");
+	makeMove(board, X, "a8");
 	printBoard(board);
-	cout<<(makeMove(board, O, "c20")== 0 ? "invalid move" : "valid move")<<endl;
-	printBoard(board);
+	if(checkForWinner(board) != 0)
+		cout<<"we have a winner!\n";
+	else
+		cout<<"no winner\n";
 
 	return 0;
 }
@@ -56,4 +65,65 @@ bool makeMove(char board[][GRID_SIZE], char player, string move)
 	else
 		board[toupper(move[0]) - 65][move[1]- '0' - 1] = player;
 	return true;
+}
+int checkForWinner(char board[][GRID_SIZE])
+{
+	int xCounter = 0, oCounter = 0;
+	//check for horizontal wins
+	for(int i = 0; i < GRID_SIZE; ++i)
+	{
+		xCounter = 0;
+		oCounter = 0;
+		for(int j = 0; j < GRID_SIZE; ++j)
+		{
+			if(board[i][j]=='_')
+			{
+				xCounter = 0;
+				oCounter = 0;
+			}
+			else if(board[i][j]=='O')
+			{
+				++oCounter;
+				xCounter = 0;
+			}
+			if(board[i][j]=='X')
+			{
+				++xCounter;
+				oCounter = 0;
+			}
+			if(oCounter==4)
+				return 1;
+			if(xCounter==4)
+				return -1;
+		}
+	}
+	//check for vertical wins
+	for(int i = 0; i < GRID_SIZE; ++i)
+	{
+		xCounter = 0;
+		oCounter = 0;
+		for(int j = 0; j < GRID_SIZE; ++j)
+		{
+			if(board[j][i]=='_')
+			{
+				xCounter = 0;
+				oCounter = 0;
+			}
+			else if(board[j][i]=='O')
+			{
+				++oCounter;
+				xCounter = 0;
+			}
+			if(board[j][i]=='X')
+			{
+				++xCounter;
+				oCounter = 0;
+			}
+			if(oCounter==4)
+				return 1;
+			if(xCounter==4)
+				return -1;
+		}
+	}
+	return 0;
 }
